ff0d5966b2d0920a1faaf382b2f2bff128017ed2
CodePriyansh <saipriyanshu786@gmail.com>
2022-04-21 06:49:00 +0530

    pay

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser')
 const path = require('path')
 const app = express()
 const mongoose = require('mongoose')
+const ejs = require('ejs')
 mongoose.connect("mongodb+srv://CodePriyanshu786:pathak123@mucluster.utw9l.mongodb.net/CakeLicious?retryWrites=true&w=majority")
 const cors = require('cors')
 const helmet = require('helmet')
@@ -19,7 +20,7 @@ const indexRoute = require('./routes/index')
 
 //Helmet helps you secure your Express apps by setting various HTTP headers. It's not a silver bullet, but it can help!
 app.use(helmet())
-
+app.set('view engine', 'ejs');
 app.use(express.json({limit : '50MB'}))
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(bodyParser.urlencoded({extended: true}))
